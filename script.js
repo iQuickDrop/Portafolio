ab45d7bb6937b1ec1a129f6dbed62e14ddc44891
iQuickDrop <lmarraqueta@gmail.com>
2026-08-02 16:59:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+// Animación al cargar
 
+window.addEventListener("load",()=>{
+
+    document.querySelector(".hero").style.opacity="1";
+
+});
+
+
+// Scroll suave
+
+document.querySelectorAll("a").forEach(link=>{
+
+    link.addEventListener("click",function(e){
+
+        if(this.hash){
+
+            e.preventDefault();
+
+            document.querySelector(this.hash)
+            .scrollIntoView({
+                behavior:"smooth"
+            });
+
+        }
+
+    });
+
+});
